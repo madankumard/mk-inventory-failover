715e0403d9ba0810899a53ffe6ec49c9b9a5a7a5
MadanKumar <madan739@gmail.com>
2020-07-12 15:03:59 +0530

    Failover for spring cloud gateway

---
+++ src/main/java/mk/springframework/mkinventoryfailover/MkInventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package mk.springframework.mkinventoryfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkInventoryFailoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkInventoryFailoverApplication.class, args);
+	}
+
+}
